858a107b77103b6a068c2616e19b526e9b75d937
KristelDi <Dezi2004@rambler.ru>
2015-11-02 21:54:22 +0300

    start

---
+++ hello.py
@@ -0,0 +1,12 @@
+def application(environ, start_response):
+	post_env = environ.copy()
+	data = "HELLO, WORLD! \n\nGET data:\n" + post_env['QUERY_STRING'] + "\nPOST data:";
+	post_env['QUERY_STRING'] = ''
+	post = environ['wsgi.input'].read(int(environ.get('CONTENT_LENGTH', '0')))
+	data += post
+	data += '\n'
+	start_response("200 OK", [
+	("Content-Type", "text/plain"),
+	("Content-Length", str(len(data))),
+	])
+	return  iter([data])
